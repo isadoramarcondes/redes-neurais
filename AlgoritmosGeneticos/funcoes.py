428b965d4a8d9b774d1914c6e31d75d81ce3d076
Isadora Marcondes A. F. Lopes <isadora220051@ilum.cnpem.br>
2023-03-09 17:14:10 -0300

    atualizando aulas 1, 2 e funcoes

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,1 +1,10 @@
- 
+def func_obj_cb(individuo):
+    """ Computa a função objetiva no problema das caixas binárias.
+    
+    Args:
+        indivíduo: lista contendo os genes das caixas binárias
+        
+    return:
+        Um valor representando a soma dos genes do indivíduo.
+    """
+    return sum(individuo) 
